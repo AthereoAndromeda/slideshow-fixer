5fa728104073785ac6e1bc99be45f82204953c32
AthereoAndromeda <athereoandromeda@gmail.com>
2022-10-11 21:01:39 +0800

    Add some funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use std::{
     fs::{self, DirEntry},
+    io::Cursor,
     path::Path,
 };
 
 mod zip;
 pub use crate::zip::*;
 
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::prelude::*;
 
 // #[wasm_bindgen]
 pub fn write_files(entries: &Vec<DirEntry>, output_path: &Path) -> Result<(), std::io::Error> {
@@ -21,7 +22,24 @@ pub fn write_files(entries: &Vec<DirEntry>, output_path: &Path) -> Result<(), st
     Ok(())
 }
 
+// Import the `window.alert` function from the Web.
 #[wasm_bindgen]
-pub fn js_write_files(file: &[u8]) -> u8 {
-    file[0]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn aaa() {
+    alert("amongus");
+}
+
+#[wasm_bindgen]
+pub fn js_write_files(file: &[u8]) -> Box<[u8]> {
+    let zip_file = Cursor::new(file);
+    let extracted = zip_main(zip_file).unwrap();
+    let inner = extracted.into_inner();
+
+    alert("woaah");
+
+    inner.into_boxed_slice()
 }
