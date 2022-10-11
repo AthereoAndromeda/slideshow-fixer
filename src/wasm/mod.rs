8455000a67d7a83f57b2dca626b8595c62232a6b
AthereoAndromeda <athereoandromeda@gmail.com>
2022-10-11 22:23:38 +0800

    Remove testing artifacts

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -9,26 +9,15 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn aaa() {
-    alert("amongus");
-}
-
-#[wasm_bindgen]
 pub fn init_module() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
 #[wasm_bindgen]
 pub fn js_write_files(file: &[u8]) -> Box<[u8]> {
-    // alert(format!("{:?}", file).as_str());
-    let a = file.len().to_string();
-    alert(a.as_str());
-
     let zip_file = Cursor::new(file);
     let extracted = zip_main(zip_file).unwrap();
     let inner = extracted.into_inner();
-
-    alert("woaah");
 
     inner.into_boxed_slice()
 }
