3bb1e276d512ceeb636f10ef9fb9aab2c5ade473
AthereoAndromeda <athereoandromeda@gmail.com>
2022-10-31 18:37:14 +0800

    Minor Refactoring

--- src/zip/archive.rs
+++ src/zip/archive.rs
@@ -1,9 +1,30 @@
-use chrono::{Datelike, Timelike};
+use chrono::{Datelike, Timelike, Utc};
 use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
 use super::MyFile;
 use super::MyZipError;
 use std::io::{Cursor, Write};
+
+/// Helper function to both convert `chrono::DateTime` to `zip::DateTime`
+/// and adds `i` seconds to `DateTime`.
+///
+/// This helps sort files by `Date Modified`.
+fn get_date_time(date_time: chrono::DateTime<Utc>, i: usize) -> zip::DateTime {
+    // Increment `Date Modified` by 1 second for each successive file
+    let date_time = date_time + chrono::Duration::seconds(i as i64);
+
+    // Convert chrono::DateTime to zip::DateTime since the zip lib
+    // im using does not support chrono
+    zip::DateTime::from_date_and_time(
+        date_time.year() as u16,
+        date_time.month() as u8,
+        date_time.day() as u8,
+        date_time.hour() as u8,
+        date_time.minute() as u8,
+        date_time.second() as u8,
+    )
+    .unwrap()
+}
 
 pub fn zip_archive(files: &Vec<MyFile>) -> Result<Box<[u8]>, MyZipError> {
     let buffer = Cursor::new(Vec::new());
@@ -13,24 +34,10 @@ pub fn zip_archive(files: &Vec<MyFile>) -> Result<Box<[u8]>, MyZipError> {
     let base_options = FileOptions::default().compression_method(CompressionMethod::Deflated);
 
     for i in 0..files.len() {
-        let file = &files[i];
-
-        // Increment `Date Modified` by 1 second for each successive file
-        let date_time = date_time + chrono::Duration::seconds(i as i64);
-
-        // Convert chrono::DateTime to zip::DateTime since the zip lib
-        // im using does not support chrono
-        let zip_date_time = zip::DateTime::from_date_and_time(
-            date_time.year() as u16,
-            date_time.month() as u8,
-            date_time.day() as u8,
-            date_time.hour() as u8,
-            date_time.minute() as u8,
-            date_time.second() as u8,
-        )
-        .unwrap();
-
+        let zip_date_time = get_date_time(date_time, i);
         let options = base_options.last_modified_time(zip_date_time);
+
+        let file = &files[i];
         zip_writer.start_file(&file.name, options)?;
         zip_writer.write_all(&file.buf)?;
     }
