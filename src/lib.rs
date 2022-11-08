24896dc98d307ffdcbb1a6c50a4573f5328e12aa
AthereoAndromeda <athereoandromeda@gmail.com>
2022-11-08 23:47:34 +0800

    refactor(threads/cli): Refactor imports and add error handling for threads

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,12 @@ pub use crate::zip::*;
 mod wasm;
 
 #[cfg(not(target_arch = "wasm32"))]
-use std::{
-    fs::{self, DirEntry},
-    path::PathBuf,
-};
+use std::{fs::DirEntry, io, path::PathBuf};
 
 #[cfg(not(target_arch = "wasm32"))]
-pub fn write_files(entries: Vec<DirEntry>, output_path: PathBuf) -> Result<(), std::io::Error> {
+pub fn write_files(entries: Vec<DirEntry>, output_path: PathBuf) -> io::Result<()> {
     use filetime::{set_file_mtime, FileTime};
-    use std::{sync::Arc, thread};
+    use std::{fs, sync::Arc, thread};
 
     let mut handles = Vec::new();
     let output_path_arc = Arc::new(output_path);
@@ -24,19 +21,23 @@ pub fn write_files(entries: Vec<DirEntry>, output_path: PathBuf) -> Result<(), s
     for (i, entry) in entries.into_iter().enumerate() {
         let output_path_counter = Arc::clone(&output_path_arc);
 
-        let handle = thread::spawn(move || {
+        let handle = thread::spawn(move || -> io::Result<()> {
             let new_path = output_path_counter.join(entry.file_name());
 
-            let content = fs::read(entry.path()).unwrap();
-            fs::write(&new_path, content).unwrap();
+            let content = fs::read(entry.path())?;
+            fs::write(&new_path, content)?;
 
             let new_time = FileTime::from_unix_time(now + i as i64, 0);
-            set_file_mtime(new_path, new_time).unwrap();
+            set_file_mtime(new_path, new_time)?;
+            Ok(())
         });
 
         handles.push(handle);
     }
 
-    handles.into_iter().for_each(|h| h.join().unwrap());
+    for handle in handles {
+        handle.join().unwrap()?;
+    }
+
     Ok(())
 }
