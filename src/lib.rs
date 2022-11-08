1d5f3ee62594430e27ae42441e17ead571139000
AthereoAndromeda <athereoandromeda@gmail.com>
2022-11-08 23:12:14 +0800

    fix(threads/cli): Fix `Date Modified` attribute to fix sorting

--- src/lib.rs
+++ src/lib.rs
@@ -12,18 +12,26 @@ use std::{
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn write_files(entries: Vec<DirEntry>, output_path: PathBuf) -> Result<(), std::io::Error> {
+    use filetime::{set_file_mtime, FileTime};
     use std::{sync::Arc, thread};
+
     let mut handles = Vec::new();
     let output_path_arc = Arc::new(output_path);
 
-    for entry in entries {
+    let now = FileTime::now();
+    let now = now.unix_seconds();
+
+    for (i, entry) in entries.into_iter().enumerate() {
         let output_path_counter = Arc::clone(&output_path_arc);
 
         let handle = thread::spawn(move || {
             let new_path = output_path_counter.join(entry.file_name());
 
             let content = fs::read(entry.path()).unwrap();
-            fs::write(new_path, content).unwrap();
+            fs::write(&new_path, content).unwrap();
+
+            let new_time = FileTime::from_unix_time(now + i as i64, 0);
+            set_file_mtime(new_path, new_time).unwrap();
         });
 
         handles.push(handle);
