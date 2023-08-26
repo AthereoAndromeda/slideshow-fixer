fa690bdf6d282379580132ef6c55fafdc70d0cfe
AthereoAndromeda <athereoandromeda@gmail.com>
2023-08-27 00:27:20 +0800

    feat: Add and use thiserror

--- src/zip/my_error.rs
+++ src/zip/my_error.rs
@@ -1,35 +1,12 @@
-use std::{error, fmt, io};
+use std::io;
 use zip::result::ZipError;
 
-#[derive(Debug)]
-/// Custom Error for program
+#[derive(Debug, thiserror::Error, displaydoc::Display)]
+/// Custom Error for .zip files
 pub enum MyZipError {
     /// Errors from `zip` lib
-    ZipError(ZipError),
+    ZipError(#[from] ZipError),
 
     /// I/O related errors
-    IoError(io::Error),
+    IoError(#[from] io::Error),
 }
-
-impl From<ZipError> for MyZipError {
-    fn from(err: ZipError) -> Self {
-        Self::ZipError(err)
-    }
-}
-
-impl From<io::Error> for MyZipError {
-    fn from(err: io::Error) -> Self {
-        Self::IoError(err)
-    }
-}
-
-impl fmt::Display for MyZipError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::IoError(io) => write!(f, "IOError: {}", io.kind()),
-            Self::ZipError(err) => write!(f, "ZipError: {err}"),
-        }
-    }
-}
-
-impl error::Error for MyZipError {}
